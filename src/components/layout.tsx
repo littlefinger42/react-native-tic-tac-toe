b637eca94b2eca4f5928717373c164c1114d37df
Laurie Copley <lauriemail@protonmail.com>
2021-08-26 16:14:32 +0200

    Remove color css from container styling on layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -22,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
-    color: colors.text,
     alignItems: "center",
     justifyContent: "center",
   },
